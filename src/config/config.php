e7d9c00c20f03d9de55cfa71a038f9984f2a15b2
Gilbert Pellegrom <gilbert@pellegrom.me>
2024-10-09 22:03:22 +0100

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,18 @@
 <?php
 
 return [
+    /**
+     * Enable or disable the canonical logger.
+     */
     'enabled' => env('CANONICAL_LOGGER_ENABLED', true),
 
+    /**
+     * The log channel to use for the canonical logger.
+     */
     'log_channel' => env('CANONICAL_LOGGER_LOG_CHANNEL', env('LOG_CHANNEL', 'stack')),
 
+    /**
+     * The log level to use for the canonical logger.
+     */
     'log_level' => env('CANONICAL_LOGGER_LOG_LEVEL', 'info'),
 ];
